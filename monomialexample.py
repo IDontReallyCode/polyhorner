d5e9149e4e9219406657f6af7f1c084594b3ddc8
IDontReallyCode <92130644+IDontReallyCode@users.noreply.github.com>
2022-04-19 08:59:44 -0500

    Update monomialexample.py

--- monomialexample.py
+++ monomialexample.py
@@ -3,9 +3,10 @@ import numpy as np
 
 
 def main():
-    X = np.array([1, 2, 3, 4, 6, 10], dtype=float)
-    t = polyhorner.horner(X, 3)
-    print(t)
+    # polynomial of order 3 in x
+    Mat = np.array([1, 2, 3, 4, 6, 10], dtype=float)
+    X = polyhorner.horner(Mat, 3)
+    print(X['X'])
 
 
 if __name__ == '__main__':
